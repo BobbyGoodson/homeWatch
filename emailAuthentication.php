6f3ef090712760f88764b65858eba9fbfad4dfff
dsalimi1000 <67076009+dsalimi1000@users.noreply.github.com>
2022-03-21 22:42:38 -0400

    Revert "Revert "Revert "Merge branch 'working' of https://github.com/BobbyGoodson/homeWatch into working"""
    This reverts commit 8062f1bc2fb83ebf47cc26d60ffc72fbf300d4d0.

--- emailAuthentication.php
+++ emailAuthentication.php
@@ -3,11 +3,8 @@
  * Email Authentication 
  */
 
-session_start();
-session_cache_expire(30);
-include_once('database/dbPersons.php');
-include_once('sendEmailFunction.php');
-//include('emailAuthenticationForm.inc');
+	session_start();
+	session_cache_expire(30);
 ?>
 <html>
 	<head>
@@ -16,51 +13,62 @@ include_once('sendEmailFunction.php');
 		</title>
 		<link rel="stylesheet" href="styles.css" type="text/css" />
 	</head>
-	<body id="bodyForm">
+	<body>
 		<div id="container">
-			<div id="form">
-				<?php
-				include('emailAuthenticationForm.inc');
-				
-				// store submit button in variable
-				$submit = $_POST['submit'];
-				// store email entered in text field in variable
-				$userEmail = $_POST['email'];
-				// create SESSION 'emailaddress' and store email in it
-				$_SESSION['emailaddress'] = $userEmail;
-	
-				// if $_SESSION['generatedCode'] is not set
-                if (!isset($_SESSION['generatedCode'])){
-				    
-					// call generate_code() function and store return value in session
-                    $_SESSION['generatedCode'] = generate_code();
-                }
+			<?PHP include('header.php');?>
+			<div id="content">
+				<?PHP
+				/*
+				sends email
+				*/
+				function email_send($email){
+					//$to = $email;
+         			$subject = "YMCA Child Watch Eemail Verification";
+         			$message = "<b>http://localhost/homeWatch/personEdit.php</b>";
+         			$header = "From:tdrink24@gmail.com \r\n"; //this is my email but you may change it, i dont know how to make it work
+         			//$header .= "Cc:afgh@somedomain.com \r\n";
+         			$header .= "MIME-Version: 1.0\r\n";
+         			$header .= "Content-type: text/html\r\n";
+         			$retval = mail ($email,$subject,$message,$header);
+         			if( $retval == true ) {
+            			echo "Message sent successfully...";
+         			}else {
+            			echo "Message could not be sent...";
+         			}
+				}
 
-				// if email field is not left empty AND submit button is pressed
-				if ($userEmail != "" && $submit) {
-	
-					// check if there's already an entry
-					$dup = retrieve_person($userEmail);
-					// if there is
-					if ($dup)
-						// show error message
-						echo('<p class="error">Error: Unable to create an account. ' . 'The email address "' . $userEmail . '" is already in use.');
-					else {
-						// redirect to email authentication code page
-						// redirect to entering the code portion of email authentication
-						echo "<script type=\"text/javascript\">window.location = \"emailAuthenticationCode.php\";</script>";
-						// send code to email
-						email_send($userEmail, $_SESSION['generatedCode']);
+				/*
+				generate a random code
+				*/
+				/*function generate_code(){
+					$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+					$code = '';
+					for ($i = 0; $i < 6; $i++) {
+						$code .= $characters[rand(0, 61)];
 					}
-				}
-				// if email field is left empty AND submit button is pressed
-				else if ($userEmail == "" && $submit) {
+					return $code;
+				}*/
 
-					// show error message
-					echo('<p class="error">Error: Didn\'t enter an email address.');
+
+				echo('<p><strong>Email Authentication</strong><br /><br />');
+				echo('<p>To create an account, please enter and verify your email address. Click the link sent to you via email. </p>');
+               	echo('<p>Do not see it? Try looking in your spam folder or sending it again by pressing Resend below. </p>');
+
+            	echo('<p><table><form method="post"><input type="hidden" name="_submit_check" value="true">
+				<tr><td>Email:</td><td><input type="text" name="email" tabindex="1"></td></tr>
+            	<tr><td colspan="2" align="left"><input type="submit" name="resend" value="resend"></td>
+            	<td colspan="2" align="right"><input type="submit" name="submit" value="submit"></td></tr></table></p>');
+
+				$submit = $_POST['submit'];
+				$resend = $_POST['resend'];
+				if ($submit || $resend){
+					$userEmail = $_POST['email'];
+					email_send($userEmail);
 				}
+
 				?>
 			</div>
+		</div>
 		</div>
 	</body>
 </html>
