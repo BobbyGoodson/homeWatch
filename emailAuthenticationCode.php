60c7c8c3854325b0479f1478ac79aca9c63f12e2
salthespriter <salthespriter@gmail.com>
2022-03-21 22:51:08 -0400

    WE BACK BABY

---
+++ emailAuthenticationCode.php
@@ -0,0 +1,59 @@
+<?php
+/*
+ * Email Authentication step 2
+ */
+	
+session_start();
+session_cache_expire(30);
+include_once('sendEmailFunction.php');
+//include('emailCode.inc');
+?>
+<html>
+	<head>
+		<title>
+			Email Authentication step 2: enter code
+		</title>
+		<link rel="stylesheet" href="styles.css" type="text/css" />
+	</head>
+	<body id="bodyForm">
+		<div id="container">
+			<div id="form">
+				<?php
+				include('emailCode.inc');
+				
+				// if $_SESSION['generatedCode'] is not set
+                if (!isset($_SESSION['generatedCode'])){
+
+					// call generate_code() function and store return value in session
+                    $_SESSION['generatedCode'] = generate_code();
+                }
+
+				$submit2 = $_POST['submit2'];
+				$resend = $_POST['resend'];
+				$userCode = $_POST['userCode'];
+
+				// if submit2 button is pressed
+				if ($submit2){
+					// if userCode is equal to generated code
+					if ($userCode == $_SESSION['generatedCode']){
+
+						// redirect to create account page
+						echo "<script type=\"text/javascript\">window.location = \"personEdit.php?id=new\";</script>";
+						// unset $_SESSION['generatedCode'] - so that it doesn't keep sending the same code to the next person
+						unset($_SESSION['generatedCode']);
+					} else {
+						echo('<p class="error">Error: The code "' . $userCode . '" is invalid.');
+					}
+				// else if resend button is pressed
+				} else if ($resend){
+					
+					// store new generated code in session
+					$_SESSION['generatedCode'] = generate_code();
+					// resend code to email
+					email_send($_SESSION['emailaddress'], $_SESSION['generatedCode']);
+				}
+				?>
+			</div>
+		</div>
+	</body>
+</html>
