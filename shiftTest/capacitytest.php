90967b065afcd2f1343e29815455711b6fca0c5c
unknown <skibob80@gmail.com>
2022-03-30 23:18:10 -0400

    time zone fixed

--- shiftTest/capacitytest.php
+++ shiftTest/capacitytest.php
@@ -45,10 +45,9 @@
 	//TEST OF PRINTING the slots for the current day
 
 	//set time zone
-	date_default_timezone_set('America/Richmond');
+	date_default_timezone_set('America/New York');
 	//get the current day of the week (this is returned as a number with Monday being 1 Tuesday being 2 ect
 	$date = date('w');
-	$tmrw = $date + 1;
 	
 
 
