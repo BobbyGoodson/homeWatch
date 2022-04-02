fc12abc75a0e62adc9b6391c768c88e1800b4291
love2drinkwater <tdrink24@gmail.com>
2022-04-02 17:08:29 -0400

    comments

--- reserve.php
+++ reserve.php
@@ -15,15 +15,22 @@ session_cache_expire(30);
     <body>
         <?php
         include_once('database/dbShiftsNew.php');
+        //First, retrieve the children attached to this account
 
+
+
+        //Second, deal with the reserved spaces: check if there is enough space for the number of children
         $day = intval($_GET['day_num']);
         $time = intval($_GET['time']);
-        //$reserveAttempt = increment_reserved($num_children, $day, $time);
         $reserveAttempt = increment_reserved(1, $day, $time);
 
         if ($reserveAttempt == false){
             echo('failed');
         }
+
+        //Third, put these children along with the chosen time slot into the children_in_slots table
+
+        
 
         header("Location: http://localhost/homeWatch/index.php");
         ?>
