8bc943ce61f1387f8bbbeed5afa45971b07378e4
salthespriter <salthespriter@gmail.com>
2022-04-03 05:10:50 -0400

    Every child's first name is listed to the guardian's update account page

--- childEdit.php
+++ childEdit.php
@@ -30,21 +30,37 @@ include_once('database/dbLog.php');
     <body id="bodyForm">
             <div id="formperson">
                 <?PHP
-                //include('personValidate.inc');
+                include('personValidate.inc');
                 if ($_POST['_submit_child'] != 1){
                     //in this case, the form has not been submitted, so show it
                     include('childForm.inc');
                 } else {
-                    $fname = $_POST['first_name'];
-                    //in this case, the form has been submitted, so validate it
 
-                    // LASTLY: this was a successful form submission; update the database and exit
-                    process_child($child);
+                    // //cancel button - go back to their update account page
+                    // if (isset($_POST['cancel'])){
+                    //     echo "<script type=\"text/javascript\">window.location = \"personEdit.php?"  . $_SESSION['_id'] . "\";</script>";
+                    // }
 
-                    //go back to the guardians update account page
-                    //echo "<script type=\"text/javascript\">window.location = \"personEdit.php?"  . $_SESSION['_id'] . "\";</script>";
+                    // //validate the form
+                    // //in this case, the form has been submitted, so validate it
+                    // $errors = validate_form($child);  //step one is validation.
+                    // // errors array lists problems on the form submitted
+                    // if ($errors) {
+                    //     // display the errors and the form to fix
+                    //     show_errors($errors);
+                    //     if ($child->get_first_name()=="new"){
+                    //         include('personForm.inc');
+                    //     } else {
+                    //         include('childForm.inc');
+                    //     }
+                    //     die();
+                    // }
+                    //else this was a successful form submission; add child to the database and redirect to update account page
+                    //else{
+                        process_child($child);
+                        echo "<script type=\"text/javascript\">window.location = \"childEdit.php?"  . $_SESSION['_id'] . "\";</script>";  
+                    //}
                 }
-
                 
                 /**
                  * process_form sanitizes data, concatenates needed data, and enters it all into a database
@@ -58,11 +74,13 @@ include_once('database/dbLog.php');
                    $health_requirements = trim(str_replace('\\\'', '\'', htmlentities($_POST['health_requirements'])));
                    $parent_email = $_SESSION['_id'];
 			 
+                   // child first name saved in session variable to be added to update account page
+                   $_SESSION['child_first_name'] = $first_name;
+
                    $newchild = new Child($first_name, $last_name, $DOB, $health_requirements, $parent_email);
                    add_child($newchild);
                    
-                   echo "<script type=\"text/javascript\">window.location = \"personEdit.php?"  . $_SESSION['_id'] . "\";</script>";
-                    
+                   echo "<script type=\"text/javascript\">window.location = \"personEdit.php?"  . $_SESSION['_id'] . "\";</script>";           
                 }
                 ?>
             </div>
