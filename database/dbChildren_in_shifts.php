0b0be4e23739ab916800dd6ed5949b9361ace8e3
EmperorQuacken <67125803+EmperorQuacken@users.noreply.github.com>
2022-04-03 18:37:23 -0400

    Updated getall_currently reserved

--- database/dbChildren_in_shifts.php
+++ database/dbChildren_in_shifts.php
@@ -1,54 +1,59 @@
 <?php
 /*
- * Children reserved in time slots
+ * Copyright 2013 by Jerrick Hoang, Ivy Xing, Sam Roberts, James Cook, 
+ * Johnny Coster, Judy Yang, Jackson Moniaga, Oliver Radwan, 
+ * Maxwell Palmer, Nolan McNair, Taylor Talmage, and Allen Tucker. 
+ * This program is part of RMH Homebase, which is free software.  It comes with 
+ * absolutely no warranty. You can redistribute and/or modify it under the terms 
+ * of the GNU General Public License as published by the Free Software Foundation
+ * (see <http://www.gnu.org/licenses/ for more information).
  * 
  */
 
+/**
+ * @version March 1, 2012
+ * @author Oliver Radwan and Allen Tucker
+ */
 include_once('dbinfo.php');
 
 /*
- * make all four new entries into the table, adding a child for the 4 30 minute time slots
+ * add a child to dbChildren table: if already there, return false
  */
+
 function add_entry($childID, $shift_day, $shift_start_time) {
     $con=connect();
-    $query = "SELECT * FROM children_in_shifts WHERE child_id = '" . $childID . "' AND shift_day = '" . $shift_day . "' AND shift_start_time = '" . $shift_start_time . "'";
+    $query = "SELECT * FROM dbChildren WHERE id = '" . $child->get_id() . "'";
     $result = mysqli_query($con,$query);
     //if there's no entry for this id, add it
     if ($result == null || mysqli_num_rows($result) == 0) {
-        mysqli_query($con,'INSERT INTO children_in_shifts VALUES("' .
-                $childID . '","' .
-                $shift_day . '","' .
-                $shift_start_time . '");');							
+        mysqli_query($con,'INSERT INTO dbChildren VALUES("' .
+                $child->get_id() . '","' .
+                $child->get_first_name() . '","' .
+                $child->get_last_name() . '","' .
+                $child->get_DOB() . '","' .
+                $child->get_health_requirements() . '","' .
+                $child->get_parent_email() .
+                '");');							
         mysqli_close($con);
         return true;
     }
     mysqli_close($con);
     return false;
-}
+}   
 
-/*
- * check if a child is in any reserved space
- */
-function check_if_child_reserved($childID){
+// gets all children currently reserved 
+function getall_currently_reserved($status, $type, $venue) {
     $con=connect();
-    $query = "SELECT child_id FROM children_in_shifts WHERE child_id = '" . $childID . "'";
+    $query = "SELECT * FROM children_in_shifts = '" . "'";
     $result = mysqli_query($con,$query);
-    //if there's no entry for this id, add it
-    if ($result == null || mysqli_num_rows($result) == 0) {
+    if($result == null || mysqli_num_rows($result) == 0) {
         mysqli_close($con);
         return false;
     }
-    mysqli_close($con);
-    return true;
-}
-
-// gets all children currently reserved 
-/*function getall_currently_reserved($status, $type, $venue) {
-    $con=connect();
-    $query = ;
     $result = mysqli_query($con,$query);
+    $shift = array()
     mysqli_close($con);
     return $result;
-}*/
+}
 
 ?>
